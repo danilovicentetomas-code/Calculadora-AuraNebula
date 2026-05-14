a5a87294eb0f6000ff0c12b45d9f3ae44f19174e
danilovicentetomas-code <danilovicentetomas@gmail.com>
2026-05-14 16:46:18 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,15 +1,25 @@
-
+resultado = 0 
+print ("elija una entre divicion , multiplicasion  (agregar suma y resta )")
 def suma(a, b):
     return a + b
 
 def resta(a, b):
     return a - b
 
-def multiplicacion(a, b):
-    pass
+resultado = 0 
+print ("elija una entre divicion y multiplicasion")
+def divicion ():
+ a = int (input ("escrive el primer numero "))
+ b = int (input ("escrive el segudo numero"))
+ resultado = a/b
+ return (resultado)
 
-def division(a, b):
-    pass
+def multiplicasion ():
+ a = int (input ("escrive el primer numero"))
+ b = int (input ("escrive el segundo numero"))
+ resultado = a * b
+ return (resultado)
+
 
 if __name__ == "__main__":
     print("Suma:", suma(10, 5))
