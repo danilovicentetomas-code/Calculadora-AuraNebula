4a2cbf9c9d7e32afa144e72fe65fcaf575ff701d
El-chabobi <schiavonibautista17@gmail.com>
2026-05-14 17:06:07 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,22 +1,31 @@
 resultado = 0 
-print ("elija una entre divicion , multiplicasion  (agregar suma y resta )")
-def suma(a, b):
-    return a + b
-
-def resta(a, b):
-    return a - b
+print ("elija una entre divicion, multiplicasion, suma o resta")
+a=int(input("Ingrese un numero: "))
+b=int(input("Ingrese otro numero: "))
+def suma():
+    a+b
+    resultado=a+b
+    return(resultado)
+def resta():
+    a-b
+    resultado=a-b
+    return(resultado)
+print("El resultado de la suma es: ")
+print(a+b)
+print("EL resultado de la resta es: ")
+print(a-b)
 
 resultado = 0 
 print ("elija una entre divicion y multiplicasion")
 def divicion ():
- a = int (input ("escrive el primer numero "))
- b = int (input ("escrive el segudo numero"))
+ a = int (input ("escribe el primer numero "))
+ b = int (input ("escribe el segudo numero"))
  resultado = a/b
  return (resultado)
 
 def multiplicasion ():
- a = int (input ("escrive el primer numero"))
- b = int (input ("escrive el segundo numero"))
+ a = int (input ("escribe el primer numero"))
+ b = int (input ("escribe el segundo numero"))
  resultado = a * b
  return (resultado)
 
