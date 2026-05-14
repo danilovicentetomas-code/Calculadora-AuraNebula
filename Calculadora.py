fc2f462e23117a8495d320f19ec9e42ac8cb7800
danilovicentetomas-code <danilovicentetomas@gmail.com>
2026-05-14 15:30:24 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    pass
+
+def division(a, b):
+    pass
+
+if __name__ == "__main__":
+    print("Suma:", suma(10, 5))
+    print("Resta:", resta(10, 5))
